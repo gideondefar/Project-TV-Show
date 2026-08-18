592bb3f759f3c3c10e5c2dde1dad26dceb943e54
vmoratti <vmoratti@gmail.com>
2026-08-18 15:01:51 +0100

    refactor and add search functionality

--- script.js
+++ script.js
@@ -1,33 +1,56 @@
-const allEpisodes = getAllEpisodes();
+//const allEpisodes = getAllEpisodes();
 
 const root = document.getElementById("root");
+const state = {
+  allEpisodes: getAllEpisodes(),
+  searchTerm: "",
+};
+function render() {
+  const filteredFilms = state.allEpisodes.filter((film) =>
+    film.title.includes(state.searchTerm),
+  );
+  const filmCards = filteredFilms.map(createFilmCard);
+  document.body.append(...filmCards);
+}
 
-root.innerHTML = allEpisodes
-  .map((ep) => {
-    const seasonNumber = String(ep.season).padStart(2, "0"); // 01
-    const episodeNumber = String(ep.number).padStart(2, "0");
+function createEpisodeCard(ep) {
+  const seasonNumber = String(ep.season).padStart(2, "0");
+  const episodeNumber = String(ep.number).padStart(2, "0");
 
-    return `
-      <div class="card">
-        <h1 class="title">
-          S${seasonNumber}E${episodeNumber} - ${ep.name}
-        </h1>
+  return `
+    <div class="card">
+      <h1 class="title">
+        S${seasonNumber}E${episodeNumber} - ${ep.name}
+      </h1>
 
-        ${
-          ep.image?.medium
-            ? `
-              <img
-                src="${ep.image.medium}"
-                alt="${ep.name} episode poster"
-              />
-            `
-            : ""
-        }
+      ${
+        ep.image?.medium
+          ? `
+            <img
+              src="${ep.image.medium}"
+              alt="${ep.name} episode poster"
+            />
+          `
+          : ""
+      }
 
-        <div class="content">
-          ${ep.summary || "<p>No summary available.</p>"}
-        </div>
+      <div class="content">
+        ${ep.summary || "<p>No summary available.</p>"}
       </div>
-    `;
-  })
-  .join("");
+    </div>
+  `;
+}
+function render() {
+  const filteredEpisodes = state.allEpisodes.filter((episode) =>
+    episode.name.toLowerCase().includes(state.searchTerm.toLowerCase()),
+  );
+
+  root.innerHTML = filteredEpisodes.map(createEpisodeCard).join("");
+}
+render();
+const searchInput = document.getElementById("site-search");
+
+searchInput.addEventListener("input", (event) => {
+  state.searchTerm = event.target.value;
+  render();
+});
